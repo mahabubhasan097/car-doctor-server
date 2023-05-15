cac723257330571279fc5bed88768da44687041b
mahabubhasan097 <mhrafi1026@gmail.com>
2023-05-15 23:41:34 +0600

    May 15, 2023

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+//require('crypto').randomBytes(64).toString('hex')
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -29,6 +31,15 @@ async function run() {
     const serviceCollection = client.db('carDoctor').collection('services');
     const bookingCollection = client.db('carDoctor').collection('bookings');
 
+
+    //jwt 
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        res.send({token});
+    })
+
+    //services routes
     app.get('/services', async (req, res) => {
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
@@ -49,7 +60,7 @@ async function run() {
     })
 
 
-    // bookings 
+    // bookings routes
     app.get('/bookings', async (req, res) => {
         console.log(req.query.email);
         let query = {};
